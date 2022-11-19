398cbd904d5d2b1836382ec828d029a5ccb78a36
Cliff Ford <cliff.ford@gmail.com>
2022-11-19 15:02:52 -0500

    add image viewer and stuff

--- webui/src/pages/Search.tsx
+++ webui/src/pages/Search.tsx
@@ -1,31 +1,94 @@
+import PrecisionManufacturingIcon from "@mui/icons-material/PrecisionManufacturing";
+import { Alert, Fab } from "@mui/material";
 import Backdrop from "@mui/material/Backdrop";
-import Button from "@mui/material/Button";
 import CircularProgress from "@mui/material/CircularProgress";
 import TextField from "@mui/material/TextField";
-import { useCallback, useState } from "react";
-import { gridImageUrl, imageSearch } from "../backend";
+import { useCallback, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { fetchRequest, gridImageUrl, imageSearch } from "../backend";
+import ImageViewer from "../components/ImageViewer";
+import { usePrevious } from "../util/hooks";
+
+const imageList = (imageId: string) =>
+  [
+    {
+      original: `${gridImageUrl(imageId)}`,
+      thumbnail: `${gridImageUrl(imageId)}`,
+    },
+  ].concat(
+    Array.from(Array(6).keys()).map(i => ({
+      original: `${gridImageUrl(imageId)}/${i}`,
+      thumbnail: `${gridImageUrl(imageId)}/${i}`,
+    })),
+  );
 
 export const Search = () => {
+  const params = useParams();
+  const imageId = params.imageId;
   const [generating, setGenerating] = useState(false);
   const [searchText, setSearchText] = useState("");
-  const [imageId, setImageId] = useState<string>();
+  const [notFound, setNotFound] = useState<boolean>();
+  const navigate = useNavigate();
 
-  const doSearch = useCallback((searchText: string) => {
-    setGenerating(true);
-    imageSearch(searchText, gr => setImageId(gr.imageId)).finally(() => setGenerating(false));
-  }, []);
+  const oldImageId = usePrevious(imageId);
+  useEffect(() => {
+    if (oldImageId !== imageId) setNotFound(undefined);
+  }, [imageId, oldImageId]);
+
+  const doSearch = useCallback(
+    (searchText: string) => {
+      setGenerating(true);
+      imageSearch(searchText, gr => navigate(`/sd/search/${gr.imageId}`)).finally(() => setGenerating(false));
+    },
+    [navigate],
+  );
+
+  useEffect(() => {
+    if (imageId) {
+      fetchRequest(
+        imageId,
+        frr => {
+          console.log("found request");
+          setSearchText(frr.prompt);
+          setNotFound(false);
+        },
+        resp => {
+          if (resp.status === 404) {
+            console.log("couldn't find this request");
+            setNotFound(true);
+          }
+        },
+      );
+    }
+  }, [imageId]);
 
   return (
     <>
       <Backdrop sx={{ color: "text.primary", zIndex: theme => theme.zIndex.drawer + 1 }} open={generating}>
         <CircularProgress color="inherit" />
       </Backdrop>
-      <TextField label="Description of image" value={searchText} onChange={e => setSearchText(e.target.value)} />
-      <Button variant="contained" disabled={!searchText.length} onClick={() => doSearch(searchText)}>
-        Generate
-      </Button>
-      {imageId !== undefined && imageId ? (
-        <img src={gridImageUrl(imageId)} alt={`generated result for ${searchText}`} style={{ maxWidth: "100%" }} />
+      <div style={{ display: "flex", flexDirection: "row", alignItems: "center", justifyContent: "center" }}>
+        <TextField
+          label="Description of image"
+          value={searchText}
+          onChange={e => setSearchText(e.target.value)}
+          multiline
+          maxRows={4}
+          style={{ width: 500 }}
+        />
+        <Fab disabled={!searchText.length} onClick={() => doSearch(searchText)} color={"primary"} style={{ margin: 5 }}>
+          <PrecisionManufacturingIcon />
+        </Fab>
+      </div>
+      {notFound ? (
+        <div>
+          <Alert severity="warning">Request not found</Alert>
+        </div>
+      ) : (
+        <></>
+      )}
+      {imageId !== undefined && imageId && notFound !== undefined && !notFound && !generating ? (
+        <ImageViewer searchText={searchText} images={imageList(imageId)} />
       ) : (
         <></>
       )}
